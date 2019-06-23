d6ae5613813b5d58a2e58520471ea63ce82a42f3
SnejUgal <contact@snejugal.ru>
2019-06-23 12:18:08 +0700

    Remove main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-const fs = require(`fs`);
-const preview = require(`./preview-maker`);
-
-let make = async ()=>{
-  fs.writeFileSync('./test/qQQ.png',await preview.makePrev(fs.readFileSync(`./test/test2.attheme`),'MyBestTheme','@alexstrnik','./test/new-preview.svg'));
-};
-make();
