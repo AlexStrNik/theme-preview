1775df39679f3741339494140041b68c352744ec
ShaMsiDDiN <64980797+shamsiddin7@users.noreply.github.com>
2020-12-26 18:17:29 +0500

    Implement a new preview (#20)
    Co-authored-by: Andrew Lane <32808683+AndrewLaneX@users.noreply.github.com>
    Co-authored-by: SnejUgal <contact@snejugal.ru>
    Co-authored-by: AlexStrNik <alexstrnik@gmail.com>

--- bot_core.js
+++ bot_core.js
@@ -5,102 +5,208 @@ const bot = new Telegraf(token);
 const request = require(`request-promise`);
 const render = require(`./render-pool`);
 const atthemeEditorApi = require(`attheme-editor-api`);
-const { REGULAR_TEMPLATE, NEW_TEMPLATE } = require(`./preview-maker`);
-
+const {
+  MINIMALISTIC_TEMPLATE,
+  REGULAR_TEMPLATE,
+  NEW_TEMPLATE,
+} = require(`./preview-maker`);
 const RESEND_ON_ERRORS = [`RequestError`, `FetchError`];
 
 bot.context.downloadFile = async function (fileId) {
-    if (!fileId) {
-        fileId = this.message.document.file_id;
-    }
+  if (!fileId) {
+    fileId = this.callbackQuery.message.reply_to_message.document.file_id;
+  }
 
-    const file = await bot.telegram.getFile(fileId);
-    const fileContent = await request({
-        encoding: null,
-        uri: `http://api.telegram.org/file/bot${token}/${file.file_path}`,
-    });
+  const file = await bot.telegram.getFile(fileId);
+  const fileContent = await request({
+    encoding: null,
+    uri: `http://api.telegram.org/file/bot${token}/${file.file_path}`,
+  });
 
-    return fileContent;
+  return fileContent;
 };
 
 const handleStart = async (context) => {
-    const id = context.message.text.slice(`/start `.length).trim();
+  const id = context.message.text.slice(`/start `.length).trim();
 
-    if (id.length === 0) {
-        await context.reply(`Send me an .attheme file to create its preview`);
-        return;
-    }
-    const { name, theme } = await atthemeEditorApi.downloadTheme(id);
-    const preview = await render({
-        theme,
-        name,
-        template: NEW_TEMPLATE,
-    });
+  if (id.length === 0) {
+    await context.reply(`Send me an .attheme file to create its preview`);
+    return;
+  }
+  const preview = await render({
+    theme,
+    name,
+    template: NEW_TEMPLATE,
+  });
 
-    const sendPreview = async () => {
-        try {
-            await context.replyWithPhoto(
-                { source: preview },
-                { // eslint-disable-next-line camelcase
-                    reply_to_message_id: context.message.message_id,
-                    caption: `${name}\nCreated by @ThemePreviewBot`,
-                },
-            );
-        } catch(error) {
-            if (RESEND_ON_ERRORS.includes(error)) {
-                process.nextTick(sendPreview);
-            } else {
-                console.error(error);
-            }
+  const sendPreview = async () => {
+    try {
+      await context.replyWithPhoto(
+        { source: preview },
+        {
+          reply_to_message_id: context.message.message_id,
+          caption: `${name}\nCreated by @ThemePreviewBot`,
         }
-    };
+      );
+    } catch (error) {
+      if (RESEND_ON_ERRORS.includes(error)) {
+        process.nextTick(sendPreview);
+      } else {
+        console.error(error);
+      }
+    }
+  };
 
-    sendPreview();
+  sendPreview();
+};
+const choose = async (context) => {
+  const fileName = context.message.document.file_name;
+  if (fileName && fileName.endsWith(`.attheme`)) {
+    if ([`group`, `supergroup`].includes(context.message.chat.type)) {
+      bot.telegram.sendChatAction(context.message.chat.id, `upload_photo`);
+      const theme = await context.downloadFile(
+        context.message.document.file_id
+      );
+      const preview = await render({
+        theme,
+        name: fileName.replace(`.attheme`, ``),
+        template: REGULAR_TEMPLATE,
+      });
+
+      const sendPreview = async () => {
+        try {
+          await context.replyWithPhoto(
+            { source: preview },
+            {
+              // eslint-disable-next-line camelcase
+              reply_to_message_id: context.message.message_id,
+              reply_markup: {
+                inline_keyboard: [
+                  [
+                    {
+                      text: `Minimalistic`,
+                      callback_data: `minimalistic`,
+                      hide: false,
+                    },
+                  ],
+                ],
+              },
+              caption: `Created by @ThemePreviewBot`,
+            }
+          );
+        } catch (error) {
+          if (RESEND_ON_ERRORS.includes(error.name)) {
+            process.nextTick(sendPreview);
+          } else {
+            console.error(error);
+          }
+        }
+      };
+
+      sendPreview();
+    } else {
+      context.reply(`Select the style`, {
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: `Ordinary`,
+                callback_data: `ordinary`,
+                hide: false,
+              },
+              {
+                text: `Minimalistic`,
+                callback_data: `minimalistic`,
+                hide: false,
+              },
+            ],
+          ],
+        },
+        reply_to_message_id: context.message.message_id,
+      });
+    }
+  }
 };
 
 const handleDocument = async (context) => {
-    const fileName = context.message.document.file_name;
-
-    if (fileName && fileName.endsWith(`.attheme`)) {
-        const theme = await context.downloadFile();
-        const preview = await render({
-            theme,
-            name: fileName.replace(`.attheme`,``),
-            template: REGULAR_TEMPLATE,
-        });
-
-        const sendPreview = async () => {
-            try {
-                await context.replyWithPhoto(
-                    { source: preview },
-                    { // eslint-disable-next-line camelcase
-                        reply_to_message_id: context.message.message_id,
-                        caption: `Created by @ThemePreviewBot`,
-                    },
-                );
-            } catch(error) {
-                if (RESEND_ON_ERRORS.includes(error.name)) {
-                    process.nextTick(sendPreview);
-                } else {
-                    console.error(error);
-                }
-            }
-        };
-
-        sendPreview();
+  const callbackQuery = context.callbackQuery;
+  const callbackMessage = callbackQuery.message;
+  if (callbackMessage.text == `Select the style`) {
+    context.deleteMessage(callbackMessage.message_id);
+  } else {
+    bot.telegram.editMessageCaption(
+      callbackMessage.chat.id,
+      callbackMessage.message_id,
+      callbackMessage.message_id,
+      `Updating the preview...`
+    );
+  }
+  bot.telegram.sendChatAction(callbackMessage.chat.id, `upload_photo`);
+  const fileName = callbackMessage.reply_to_message.document.file_name;
+  const theme = await context.downloadFile();
+  const preview = await render({
+    theme,
+    name: fileName.replace(`.attheme`, ``),
+    template:
+      callbackQuery.data == `ordinary`
+        ? REGULAR_TEMPLATE
+        : MINIMALISTIC_TEMPLATE,
+  });
+  const sendPreview = async () => {
+    const reply_markup = {
+      inline_keyboard: [
+        [
+          {
+            text:
+              callbackQuery.data == `ordinary` ? `Minimalistic` : `Ordinary`,
+            callback_data:
+              callbackQuery.data == `ordinary` ? `minimalistic` : `ordinary`,
+            hide: false,
+          },
+        ],
+      ],
+      caption: `Created by @ThemePreviewBot`,
+    };
+    try {
+      if (callbackMessage.text == `Select the style`) {
+        await context.replyWithPhoto(
+          { source: preview },
+          {
+            reply_to_message_id: callbackMessage.reply_to_message.message_id,
+            reply_markup,
+          }
+        );
+      } else {
+        await context.editMessageMedia(
+          { type: `photo`, media: { source: preview } },
+          { reply_markup }
+        );
+      }
+    } catch (error) {
+      if (RESEND_ON_ERRORS.includes(error.name)) {
+        process.nextTick(sendPreview);
+      } else {
+        console.error(error);
+      }
     }
+  };
+  sendPreview();
 };
 
 bot.start((context) => {
-    handleStart(context);
+  handleStart(context);
 });
 
 bot.help((context) => {
-    context.reply(`Send me an .attheme file to create its preview`);
+  context.reply(`Send me an .attheme file to create its preview`);
 });
 
 bot.on(`document`, (context) => {
-    handleDocument(context);
+  choose(context);
+});
+
+bot.action([`ordinary`, `minimalistic`], (context) => {
+  handleDocument(context);
 });
 
 bot.polling.offset = -100;
--- preview-maker.js
+++ preview-maker.js
@@ -1,184 +1,377 @@
 const Attheme = require(`attheme-js`);
 const fs = require(`fs`);
-const defaultVariablesValues = require(`attheme-default-values`).default;
+const defaultVariablesValues = require(`./attheme-default-values`).default;
 const { DOMParser, XMLSerializer } = require(`xmldom`);
 const sharp = require(`sharp`);
 const sizeOf = require(`image-size`);
 const { serializeToString: serialize } = new XMLSerializer();
 const Color = require(`@snejugal/color`);
+const rgbToHsl = Color.rgbToHsl;
 
 const RENDER_CONFIG = {
-    density: 150,
+  density: 150,
 };
 
 const parser = new DOMParser();
 
+const MINIMALISTIC_TEMPLATE = Symbol();
 const REGULAR_TEMPLATE = Symbol();
 const NEW_TEMPLATE = Symbol();
 
 const templates = {
-    [REGULAR_TEMPLATE]: fs.readFileSync(`./theme-preview.svg`, `utf8`),
-    [NEW_TEMPLATE]: fs.readFileSync(`./new-preview.svg`, `utf8`),
+  [MINIMALISTIC_TEMPLATE]: fs.readFileSync(`./new-theme-preview.svg`, `utf8`),
+  [REGULAR_TEMPLATE]: fs.readFileSync(`./theme-preview.svg`, `utf8`),
+  [NEW_TEMPLATE]: fs.readFileSync(`./new-preview.svg`, `utf8`),
 };
 
 const WALLPAPERS_AMOUNT = 32;
 const wallpapers = [];
 
 for (let index = 0; index < WALLPAPERS_AMOUNT; index++) {
-    const wallpaper = fs.readFileSync(
-        `./wallpapers/${index}.jpg`,
-        `binary`,
-    );
+  const wallpaper = fs.readFileSync(`./wallpapers/${index}.jpg`, `binary`);
 
-    wallpapers.push(wallpaper);
+  wallpapers.push(wallpaper);
 }
 
-const get = (node, className, tag) => Array.from(node.getElementsByTagName(tag))
-    .filter((element) => (
-        element.getAttribute
-        && element.getAttribute(`class`) === className
-    ));
+const get = (node, className, tag) =>
+  Array.from(node.getElementsByTagName(tag)).filter(
+    (element) =>
+      element.getAttribute && element.getAttribute(`class`) === className
+  );
 
 const getElementsByClassName = (node, className) => [
-    ...get(node, className, `rect`),
-    ...get(node, className, `circle`),
-    ...get(node, className, `path`),
-    ...get(node, className, `g`),
-    ...get(node, className, `polygon`),
-    ...get(node, className, `image`),
-    ...get(node, className, `tspan`),
-    ...get(node, className, `stop`),
+  ...get(node, className, `rect`),
+  ...get(node, className, `circle`),
+  ...get(node, className, `path`),
+  ...get(node, className, `g`),
+  ...get(node, className, `polygon`),
+  ...get(node, className, `image`),
+  ...get(node, className, `tspan`),
+  ...get(node, className, `stop`),
 ];
+const calculateAccentColor = (colors) => {
+  let colorsQuantity = new Map();
+  let max = 0;
+  let accentHue;
 
-const fill = (node, color) => {
-    const { red, green, blue, alpha } = color;
-    const rgba = `rgba(${red}, ${green}, ${blue}, ${alpha / 255})`;
+  for (const color of colors) {
+    if (color != 0) {
+      colorsQuantity.set(color, (colorsQuantity.get(color) ?? 0) + 1);
 
+      if (colorsQuantity.get(color) > max) {
+        accentHue = color;
+        max = colorsQuantity.get(color);
+      }
+    }
+  }
+  const colorsHue = colors.filter((color) => colorsQuantity.get(color) == max);
+  colorsHue.sort((a, b) => a - b);
+
+  if (colorsHue.length > 1) {
+    let minDifference = 360;
+    for (let i = 0; i < colorsHue.length - 1; i++) {
+      const outerHue = colorsHue[i];
+      const innerHue = colorsHue[i + 1];
+      if (Math.abs(outerHue - innerHue) < minDifference) {
+        minDifference = Math.abs(outerHue - innerHue);
+        accentHue = (outerHue + innerHue) / 2;
+      }
+    }
+  }
+
+  return accentHue;
+}
+const rgbDifference = (color1, color2) => {
+  const result = Math.hypot(
+    color1.red - color2.red,
+    color1.green - color2.green,
+    color1.blue - color2.blue
+  );
+  return result;
+}
+const fill = (rootNode, color) => {
+  const cssColor =
+    `red` in color
+      ? `rgba(${color.red}, ${color.green}, ${color.blue}, ${
+          color.alpha / 255
+        })`
+      : `hsl(${color.hue}, ${color.saturation * 100}%, ${
+          color.lightness * 100
+        }%)`;
+
+  const innerFill = (node) => {
     if (node.tagName === `stop`) {
-        node.setAttribute(`stop-color`, rgba);
+      node.setAttribute(`stop-color`, cssColor);
     } else {
-        node.setAttribute(`fill`, rgba);
+      node.setAttribute(`fill`, cssColor);
     }
 
     if (node.childNodes) {
-        for (let child of Array.from(node.childNodes)) {
-            if (child.setAttribute) {
-                fill(child, color);
-            }
+      for (let child of Array.from(node.childNodes)) {
+        if (child.setAttribute) {
+          innerFill(child, cssColor);
         }
+      }
     }
+  };
+
+  innerFill(rootNode);
 };
 
 const makePrev = async (themeBuffer, themeName, themeAuthor, template) => {
-    let theme;
+  let theme, accentHue;
 
-    if (themeBuffer instanceof Buffer) {
-        theme = new Attheme(themeBuffer.toString(`binary`));
-    } else {
-        theme = themeBuffer;
+  if (themeBuffer instanceof Buffer) {
+    theme = new Attheme(themeBuffer.toString(`binary`));
+  } else {
+    theme = themeBuffer;
+  }
+
+  const preview = parser.parseFromString(templates[template]);
+  const AVATAR_VARIABLES = [
+    `avatar_backgroundRed`,
+    `avatar_backgroundOrange`,
+    `avatar_backgroundViolet`,
+    `avatar_backgroundGreen`,
+    `avatar_backgroundCyan`,
+    `avatar_backgroundBlue`,
+  ];
+  const windowBackgroundWhite =
+    theme[`windowBackgroundWhite`] ||
+    defaultVariablesValues[`windowBackgroundWhite`];
+
+  const inBubble =
+    theme[`chat_inBubble`] || defaultVariablesValues[`chat_inBubble`];
+  const outBubble =
+    theme[`chat_outBubble`] || defaultVariablesValues[`chat_outBubble`];
+  if (Color.brightness(inBubble) > Color.brightness(outBubble)) {
+    theme[`chat_{in/out}Bubble__darkest`] = inBubble;
+  } else {
+    theme[`chat_{in/out}Bubble__darkest`] = outBubble;
+  }
+  let colors = [];
+  for (const variable in defaultVariablesValues) {
+    if (variable === `chat_wallpaper` && !theme.chat_wallpaper) {
+      continue;
     }
+    const elements = getElementsByClassName(preview, variable);
+    const color = theme[variable] || defaultVariablesValues[variable];
+    for (const element of elements) {
+      fill(element, color);
+    }
+    const chooseHsl = rgbToHsl(color);
+    let hueDifference = Math.abs(chooseHsl.hue - rgbToHsl(windowBackgroundWhite).hue);
+    if (hueDifference > 180) {
+      hueDifference = 360 - hueDifference;
+    }
+    let saturationDifference = Math.abs(chooseHsl.saturation - windowBackgroundWhite.saturation)
+    if (
+      hueDifference > 2 &&
+      chooseHsl.saturation > 0.04 &&
+      saturationDifference > 0.02
+    ) {
+      colors.push(Math.round(chooseHsl.hue));
+    }
+  }
 
-    const preview = parser.parseFromString(templates[template]);
+  if (colors.length > 0) {
+    accentHue = calculateAccentColor(colors);
+  } else {
+    accentHue = rgbToHsl(
+      theme[`chats_actionBackground`] ||
+        defaultVariablesValues[`chats_actionBackground`]
+    ).hue;
+  }
 
-    const inBubble = (
-        theme[`chat_inBubble`]
-        || defaultVariablesValues[`chat_inBubble`]
+  for (const outBubbleGradientelement of getElementsByClassName(
+    preview,
+    `chat_outBubbleGradient`
+  )) {
+    const color =
+      theme[`chat_outBubbleGradient`] ||
+      theme[`chat_outBubble`] ||
+      defaultVariablesValues[`chat_outBubble`];
+    fill(outBubbleGradientelement, color);
+  }
+  for (const previewBackLinear of getElementsByClassName(
+    preview,
+    `PreviewBackLinear`
+  )) {
+    const chatOutBubble =
+      theme[`chat_outBubble`] || defaultVariablesValues[`chat_outBubble`];
+    fill(previewBackLinear, {
+      hue: rgbToHsl(theme[`chat_outBubbleGradient`] || chatOutBubble).hue,
+      saturation: 1,
+      lightness: 0.9,
+    });
+  }
+  for (const previewBackLinearShadow of getElementsByClassName(
+    preview,
+    `PreviewBackLinearShadow`
+  )) {
+    fill(previewBackLinearShadow, {
+      hue: rgbToHsl(
+        theme[`chat_outBubble`] || defaultVariablesValues[`chat_outBubble`]
+      ).hue,
+      saturation: 1,
+      lightness: 0.9,
+    });
+  }
+  for (const previewBack of getElementsByClassName(preview, `PreviewBack`)) {
+    fill(previewBack, { hue: accentHue, saturation: 1, lightness: 0.9 });
+  }
+  for (const chatShadow of getElementsByClassName(preview, `ChatShadow`)) {
+    fill(chatShadow, { hue: accentHue, saturation: 1, lightness: 0.02 });
+  }
+  if (`chat_outBubbleGradient` in theme) {
+    for (const previewBack of getElementsByClassName(preview, `PreviewBack`)) {
+      fill(previewBack, { red: 0, green: 0, blue: 0, alpha: 0 });
+    }
+  }
+
+  const avatarTextColor =
+    theme[`avatar_text`] || defaultVariablesValues[`avatar_text`];
+  for (const avatarVariable of AVATAR_VARIABLES) {
+    const avatarColor =
+      theme[avatarVariable] || defaultVariablesValues[avatarVariable];
+    const avatarAndBackgroundDifference = rgbDifference(
+      avatarColor,
+      windowBackgroundWhite
     );
-    const outBubble = (
-        theme[`chat_outBubble`]
-        || defaultVariablesValues[`chat_outBubble`]
-    );
-
-    if (Color.brightness(inBubble) > Color.brightness(outBubble)) {
-        theme['chat_{in/out}Bubble__darkest'] = inBubble;
+    if (avatarAndBackgroundDifference < 25) {
+      for (const avatar of getElementsByClassName(preview, avatarVariable)) {
+        fill(avatar, avatarTextColor);
+      }
+      for (const avatarShadow of getElementsByClassName(
+        preview,
+        `${avatarVariable}Shadow`
+      )) {
+        let hslChoose = rgbToHsl(avatarTextColor);
+        hslChoose.lightness += hslChoose.lightness < 0.25 ? 0.1 : -0.2;
+        fill(avatarShadow, hslChoose);
+      }
     } else {
-        theme['chat_{in/out}Bubble__darkest'] = outBubble;
+      for (const avatarShadow of getElementsByClassName(
+        preview,
+        `${avatarVariable}Shadow`
+      )) {
+        const choose =
+          theme[avatarVariable] || defaultVariablesValues[avatarVariable];
+        const hslChoose = rgbToHsl(choose);
+        hslChoose.lightness += hslChoose.lightness < 0.25 ? 0.1 : -0.2;
+        fill(avatarShadow, hslChoose);
+      }
     }
+  }
 
-    for (const variable in defaultVariablesValues) {
-        if (variable === `chat_wallpaper` && !theme.chat_wallpaper) {
-            continue;
+  const chatInLoader =
+    theme[`chat_inLoader`] || defaultVariablesValues[`chat_inLoader`];
+  const chatInBubble =
+    theme[`chat_inBubble`] || defaultVariablesValues[`chat_inBubble`];
+  const chatOutLoader =
+    theme[`chat_outLoader`] || defaultVariablesValues[`chat_outLoader`];
+  const chatOutBubble =
+    theme[`chat_outBubble`] || defaultVariablesValues[`chat_outBubble`];
+  const inLoaderAndBubbleDifference = rgbDifference(chatInLoader, chatInBubble);
+  const outLoaderAndBubbleDifference = rgbDifference(
+    chatOutLoader,
+    chatOutBubble
+  );
+  const chatInMediaIcon =
+    theme[`chat_inMediaIcon`] || defaultVariablesValues[`chat_inMediaIcon`];
+  const chatOutMediaIcon =
+    theme[`chat_outMediaIcon`] || defaultVariablesValues[`chat_outMediaIcon`];
+
+  if (inLoaderAndBubbleDifference < 25) {
+    for (const chatInLoader of getElementsByClassName(
+      preview,
+      `chat_inLoader`
+    )) {
+      fill(chatInLoader, chatInMediaIcon);
+    }
+  }
+  if (outLoaderAndBubbleDifference < 25) {
+    for (const chatOutLoader of getElementsByClassName(
+      preview,
+      `chat_outLoader`
+    )) {
+      fill(chatOutLoader, chatOutMediaIcon);
+    }
+  }
+
+  if (!theme[Attheme.IMAGE_KEY] && !theme.chat_wallpaper) {
+    const randomWallpaper = Math.floor(Math.random() * WALLPAPERS_AMOUNT);
+    const image = wallpapers[randomWallpaper];
+
+    theme[Attheme.IMAGE_KEY] = image;
+  }
+
+  const elements = getElementsByClassName(preview, `IMG`);
+
+  await Promise.all(
+    elements.map(async (element) => {
+      let chatWidth = Number(element.getAttribute(`width`));
+      let chatHeight = Number(element.getAttribute(`height`));
+      let ratio = chatHeight / chatWidth;
+
+      if (theme[Attheme.IMAGE_KEY] && !theme.chat_wallpaper) {
+        const imageBuffer = Buffer.from(theme[Attheme.IMAGE_KEY], `binary`);
+
+        const { width, height } = sizeOf(imageBuffer);
+        const imageRatio = height / width;
+
+        let finalHeight;
+        let finalWidth;
+
+        if (ratio > imageRatio) {
+          finalHeight = chatHeight;
+          finalWidth = Math.round(chatHeight / imageRatio);
+        } else {
+          finalWidth = chatWidth;
+          finalHeight = Math.round(chatWidth * imageRatio);
         }
 
-        const elements = getElementsByClassName(preview, variable);
-        const color = theme[variable] || defaultVariablesValues[variable];
+        const resizedImage = await sharp(imageBuffer)
+          .resize(finalWidth, finalHeight)
+          .png()
+          .toBuffer();
 
-        for (const element of elements) {
-            fill(element, color);
-        }
-    }
+        const croppedImage = await sharp(resizedImage)
+          .resize(chatWidth, chatHeight)
+          .png()
+          .toBuffer();
 
-    if (!theme[Attheme.IMAGE_KEY] && !theme.chat_wallpaper) {
-        const randomWallpaper = Math.floor(Math.random() * WALLPAPERS_AMOUNT);
-        const image = wallpapers[randomWallpaper];
+        element.setAttribute(
+          `xlink:href`,
+          `data:image/png;base64,${croppedImage.toString(`base64`)}`
+        );
+      }
+    })
+  );
 
-        theme[Attheme.IMAGE_KEY] = image;
-    }
+  const authorIndex = themeName.search(/ [bB]y @?[a-zA-Z0-9]/);
 
-    const elements = getElementsByClassName(preview, 'IMG');
+  if (themeAuthor === `` && authorIndex !== -1) {
+    themeAuthor = themeName.slice(authorIndex);
+    themeName = themeName.slice(0, authorIndex);
+  }
 
-    await Promise.all(elements.map(async (element) => {
-        let chatWidth = Number(element.getAttribute('width'));
-        let chatHeight = Number(element.getAttribute('height'));
-        let ratio = chatHeight / chatWidth;
+  for (const element of getElementsByClassName(preview, `theme_name`)) {
+    element.textContent = themeName;
+  }
 
-        if (theme[Attheme.IMAGE_KEY] && !theme.chat_wallpaper) {
-            const imageBuffer = Buffer.from(theme[Attheme.IMAGE_KEY], `binary`);
+  for (const element of getElementsByClassName(preview, `theme_author`)) {
+    element.textContent = themeAuthor;
+  }
 
-            const { width, height } = sizeOf(imageBuffer);
-            const imageRatio = height / width;
+  const templateBuffer = Buffer.from(serialize(preview), `binary`);
 
-            let finalHeight;
-            let finalWidth;
-
-            if (ratio > imageRatio) {
-                finalHeight = chatHeight;
-                finalWidth = Math.round(chatHeight / imageRatio);
-            } else {
-                finalWidth = chatWidth;
-                finalHeight = Math.round(chatWidth * imageRatio);
-            }
-
-            const resizedImage = await sharp(imageBuffer)
-                .resize(finalWidth, finalHeight)
-                .png()
-                .toBuffer();
-
-            const croppedImage = await sharp(resizedImage)
-                .resize(chatWidth, chatHeight)
-                .crop()
-                .png()
-                .toBuffer();
-
-            element.setAttribute(
-                `xlink:href`,
-                `data:image/png;base64,${croppedImage.toString(`base64`)}`,
-            );
-        }
-    }));
-
-    const authorIndex = themeName.search(/ [bB]y @?[a-zA-Z0-9]/);
-
-    if (themeAuthor === "" && authorIndex !== -1) {
-        themeAuthor = themeName.slice(authorIndex);
-        themeName = themeName.slice(0, authorIndex);
-    }
-
-    for (const element of getElementsByClassName(preview, `theme_name`)) {
-        element.textContent = themeName;
-    }
-
-    for (const element of getElementsByClassName(preview, `theme_author`)) {
-        element.textContent = themeAuthor;
-    };
-
-    const templateBuffer = Buffer.from(serialize(preview), `binary`);
-
-    return sharp(templateBuffer, RENDER_CONFIG).png().toBuffer();
+  return sharp(templateBuffer, RENDER_CONFIG).png().toBuffer();
 };
 
 module.exports = {
-    REGULAR_TEMPLATE,
-    NEW_TEMPLATE,
-    makePrev,
+  MINIMALISTIC_TEMPLATE,
+  REGULAR_TEMPLATE,
+  NEW_TEMPLATE,
+  makePrev,
 };
